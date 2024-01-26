bbac885fe6533ac8eee5400b8f351addfaee9650
Ahmed Wajih BEN HMIDA <awbenhmida@vermeg.com>
2024-01-26 10:04:58 +0100

    taaww

---
+++ src/main/java/tn/esprit/tpfoyer/services/IChambreService.java
@@ -0,0 +1,13 @@
+package tn.esprit.tpfoyer.services;
+
+import tn.esprit.tpfoyer.entity.Chambre;
+
+import java.util.List;
+
+public interface IChambreService {
+    public List<Chambre> retrieveAllChambres();
+    public Chambre retrieveChambre(Long chambreId);
+    public Chambre addChambre(Chambre c);
+    public void removeChambre(Long chambreId);
+    public Chambre modifyChambre(Chambre chambre);
+}
